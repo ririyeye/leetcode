db115661b9e2649f4391b651e9a6585b85f91983
ririyeye <200610237@qq.com>
2021-01-26 17:59:54 +0800

    23.cpp v3

--- 23.cpp
+++ 23.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 struct ListNode
@@ -16,59 +17,32 @@ struct ListNode
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* head1 = l1;
-        ListNode* head2 = l2;
-        ListNode temp_node;
-        ListNode* temp_ptr = &temp_node;
-        while(head1 && head2)
-        {
-            if(head1->val < head2->val)
-            {
-                temp_ptr->next = head1;
-                head1 = head1->next;
-            }
-            else
-            {
-                temp_ptr->next = head2;
-                head2 = head2->next;
-            }
-            temp_ptr = temp_ptr->next;
-        }
-        if(head1 == NULL)
-        {
-            temp_ptr->next = head2;
-        }
-        else
-        {
-            temp_ptr->next = head1;
-        }
-        return temp_node.next;
-    }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size() == 0)
             return nullptr;
         if(lists.size() == 1)
             return lists[0];
 
-        int midindex = lists.size() / 2;
+        auto cmp = [](ListNode* a, ListNode* b) {
+            if (a==NULL)
+                return true;
+            if (b==NULL)
+                return false;
+            return a->val>b->val;
+        };
+        ListNode head(0);
+        ListNode * ret = &head;
+        make_heap(lists.begin(), lists.end(), cmp);
 
-        vector<ListNode *> Lgrp;
-        vector<ListNode *> Rgrp;
-
-        for (size_t i = 0; i < lists.size(); i++)
-        {
-            if(i < midindex)
-                Lgrp.push_back(lists[i]);
-            else
-                Rgrp.push_back(lists[i]);
+        while(lists[0]!=NULL){
+            ret->next = lists[0];
+            lists[0] = lists[0]->next;
+            ret = ret->next;
+            pop_heap(lists.begin(), lists.end(), cmp);
+            push_heap(lists.begin(), lists.end(), cmp);
         }
 
-        ListNode *Llist = mergeKLists(Lgrp);
-        ListNode *Rlist = mergeKLists(Rgrp);
-
-        return merge2Lists(Llist, Rlist);
+        return head.next;
     }
 };
 
